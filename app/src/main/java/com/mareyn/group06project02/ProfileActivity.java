6634d671a277a9e18a3b9afe04235ab0e1c3ed40
XavierFRB30-alt <xavierromobriones28@gmail.com>
2025-11-15 12:55:57 -0800

    Created ProfileActivity.

---
+++ app/src/main/java/com/mareyn/group06project02/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.mareyn.group06project02;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+  // private ActivityMainBinding binding;
+
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(R.layout.activity_profile);
+    // TODO: Get username from database,
+    // TODO: Get email_address from database
+    // TODO: Get "user ID" from database
+    // TODO: Go to admin activity when admin button is pressed
+    // TODO: Make sure that the admin button is hidden when user is not admin
+  }
+}
