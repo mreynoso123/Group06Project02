a97c2f4f20677e9267265e03a7c17b771daed13d
moisesxfelix <moisesxfelix@gmail.com>
2025-12-07 14:48:58 -0800

    Added intent extras, 3 bugs fixed (hopefully).

--- app/src/main/java/com/mareyn/group06project02/ParentChoreDisplayActivity.java
+++ app/src/main/java/com/mareyn/group06project02/ParentChoreDisplayActivity.java
@@ -26,8 +26,11 @@ import com.mareyn.group06project02.viewHolders.ChoreViewModel;
 
 public class ParentChoreDisplayActivity extends AppCompatActivity {
 
+  private static final String PARENT_DISPLAY_PREVIOUS_USER_ID = "com.mareyn.group06project02.PARENT_DISPLAY_PREVIOUS_USER_ID";
+  private static final String PARENT_DISPLAY_PREVIOUS_USERNAME = "com.mareyn.group06project02.PARENT_DISPLAY_PREVIOUS_USERNAME";
   private static final String PARENT_DISPLAY_USER_ID = "com.mareyn.group06project02.PARENT_DISPLAY_USER_ID";
   private static final String PARENT_DISPLAY_USERNAME = "com.mareyn.group06project02.PARENT_DISPLAY_USERNAME";
+  private static final String PARENT_DISPLAY_CAN_EDIT = "com.mareyn.group06project02.PARENT_DISPLAY_CAN_EDIT";
 
   private ActivityParentChoreDisplayBinding binding;
   // need next line of code?
@@ -41,14 +44,25 @@ public class ParentChoreDisplayActivity extends AppCompatActivity {
   private int score = 0;
 
   // need the following info?
-  private int loggedInUserId;
+  private int userId;
   private String username;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     binding = ActivityParentChoreDisplayBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
+
+    repository = ChoreScoreRepository.getRepository(getApplication());
+    userId = getIntent().getIntExtra(PARENT_DISPLAY_USER_ID, -1);
+    username = getIntent().getStringExtra(PARENT_DISPLAY_USERNAME);
+    String previousUsername = getIntent().getStringExtra(PARENT_DISPLAY_PREVIOUS_USERNAME);
+    int previousUserId = getIntent().getIntExtra(PARENT_DISPLAY_PREVIOUS_USER_ID, -1);
+
+    if (!getIntent().getBooleanExtra(PARENT_DISPLAY_CAN_EDIT, false)) {
+      binding.createNewChoreButton.setVisibility(View.GONE);
+    }
 
     EdgeToEdge.enable(this);
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -56,9 +70,6 @@ public class ParentChoreDisplayActivity extends AppCompatActivity {
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
       return insets;
     });
-
-    loggedInUserId = getIntent().getIntExtra(PARENT_DISPLAY_USER_ID, -1);
-    username = getIntent().getStringExtra(PARENT_DISPLAY_USERNAME);
 
     // All Active Chores Recycler View
     choreViewModel = new ViewModelProvider(this).get(ChoreViewModel.class);
@@ -68,8 +79,6 @@ public class ParentChoreDisplayActivity extends AppCompatActivity {
     final ChoreAdapter adapter = new ChoreAdapter(new ChoreAdapter.ChoreDiff());
     recyclerView.setAdapter(adapter);
     recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-    repository = ChoreScoreRepository.getRepository(getApplication());
 
     choreViewModel.getAllActiveChores().observe(this, chores -> {
       adapter.submitList(chores);
@@ -88,7 +97,7 @@ public class ParentChoreDisplayActivity extends AppCompatActivity {
     });
 
     binding.goBackButton2.setOnClickListener(view -> {
-      Intent intent = LandingPageActivity.landingPageActivityIntentFactory(getApplicationContext(), username, loggedInUserId);
+      Intent intent = LandingPageActivity.landingPageActivityIntentFactory(getApplicationContext(), previousUsername, previousUserId);
       startActivity(intent);
     });
   }
@@ -138,10 +147,13 @@ public class ParentChoreDisplayActivity extends AppCompatActivity {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
   }
 
-  static Intent parentChoreDisplayActivityIntentFactory(Context context, String username, int userId) {
+  static Intent parentChoreDisplayActivityIntentFactory(Context context, String previousUsername, int previousUserId, String username, int userId, boolean canEdit) {
     Intent intent = new Intent(context, ParentChoreDisplayActivity.class);
+    intent.putExtra(PARENT_DISPLAY_PREVIOUS_USERNAME, previousUsername);
+    intent.putExtra(PARENT_DISPLAY_PREVIOUS_USER_ID, previousUserId);
     intent.putExtra(PARENT_DISPLAY_USERNAME, username);
     intent.putExtra(PARENT_DISPLAY_USER_ID, userId);
+    intent.putExtra(PARENT_DISPLAY_CAN_EDIT, canEdit);
     return intent;
   }
 }
